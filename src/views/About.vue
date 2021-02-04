2024c66c7d698ee45dc190e51b2f31741a472d9b
wangyansong <yansong_wang@foxmail.com>
2021-02-04 17:26:43 +0800

    init project

---
+++ src/views/About.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <a-date-picker @change="onChange" />
+    <a-form :model="form" :label-col="labelCol" :wrapper-col="wrapperCol">
+      <a-form-item label="Activity name">
+        <a-input v-model:value="form.name" />
+      </a-form-item>
+      <a-form-item label="Activity zone">
+        <a-select v-model:value="form.region" placeholder="please select your zone">
+          <a-select-option value="shanghai">
+            Zone one
+          </a-select-option>
+          <a-select-option value="beijing">
+            Zone two
+          </a-select-option>
+        </a-select>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      userName: null,
+      form: {
+        name: null,
+        region: null
+      },
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 }
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    onChange (date, dateString) {
+      console.log(date, dateString)
+    }
+  }
+}
+</script>
